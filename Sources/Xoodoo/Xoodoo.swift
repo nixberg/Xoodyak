aeff1532914444e065073200579279f2335c8b4d
nixberg <k.nixberg@gmail.com>
2019-09-14 09:59:19 +0200

    Refactor Xoodoo

--- Sources/Xoodoo/Xoodoo.swift
+++ Sources/Xoodoo/Xoodoo.swift
@@ -32,13 +32,7 @@ public struct Xoodoo {
     }
     
     public mutating func permute() {
-        let roundConstants = [UInt32](arrayLiteral:
-            0x058, 0x038, 0x3c0, 0x0d0,
-            0x120, 0x014, 0x060, 0x02c,
-            0x380, 0x0f0, 0x1a0, 0x012
-        )
-        
-        for c in roundConstants {
+        [0x058 as UInt32, 0x038, 0x3c0, 0x0d0, 0x120, 0x014, 0x060, 0x02c, 0x380, 0x0f0, 0x1a0, 0x012].forEach {
             // θ:
             let p = (a.0 ^ a.1 ^ a.2).rotated()
             let e = p.rotatingLanes(by: 5) ^ p.rotatingLanes(by: 14)
@@ -51,7 +45,7 @@ public struct Xoodoo {
             a.2.rotateLanes(by: 11)
             
             // ι:
-            a.0 ^= .init(c, 0, 0, 0)
+            a.0.x ^= $0
             
             // χ:
             a.0 ^= ~a.1 & a.2
@@ -60,7 +54,7 @@ public struct Xoodoo {
             
             // ρ-east:
             a.1.rotateLanes(by: 1)
-            a.2.rhoEastPart2()
+            a.2.rhoEastPartTwo()
         }
     }
 }
@@ -78,7 +72,7 @@ extension SIMD4 where Scalar == UInt32 {
     
     @inline(__always)
     func rotated() -> SIMD4<UInt32> {
-        self[.init(3, 0, 1, 2)]
+        self[SIMD4(3, 0, 1, 2)]
     }
     
     @inline(__always)
@@ -87,10 +81,10 @@ extension SIMD4 where Scalar == UInt32 {
     }
     
     @inline(__always)
-    mutating func rhoEastPart2() {
+    mutating func rhoEastPartTwo() {
         withUnsafeMutablePointer(to: &self) {
             $0.withMemoryRebound(to: SIMD16<UInt8>.self, capacity: 1) {
-                $0.pointee = $0.pointee[.init(
+                $0.pointee = $0.pointee[SIMD16(
                     11,  8,  9, 10,
                     15, 12, 13, 14,
                      3,  0,  1,  2,
