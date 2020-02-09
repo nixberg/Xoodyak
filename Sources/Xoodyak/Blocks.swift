3283028005602427f066972251d02466366a6038
nixberg <k.nixberg@gmail.com>
2020-02-09 09:37:35 +0100

    Refactor blocks iterator

--- Sources/Xoodyak/Blocks.swift
+++ Sources/Xoodyak/Blocks.swift
@@ -2,36 +2,30 @@ import Foundation
 
 extension DataProtocol {
     func blocks(rate: Int) -> Blocks<Self> {
-        Blocks<Self>(data: self, rate: rate)
+        .init(data: self, rate: rate)
     }
 }
 
-struct Blocks<D>: Sequence where D: DataProtocol{
+struct Blocks<D>: Sequence where D: DataProtocol {
+    struct BlocksIterator<D>: IteratorProtocol where D: DataProtocol {
+        var tail: D.SubSequence
+        let rate: Int
+        var firstBlock = true
+        
+        mutating func next() -> D.SubSequence? {
+            guard !tail.isEmpty || firstBlock else {
+                return nil
+            }
+            firstBlock = false
+            defer { tail = tail.dropFirst(rate) }
+            return tail.prefix(rate)
+        }
+    }
+    
     let data: D
     let rate: Int
     
     func makeIterator() -> BlocksIterator<D> {
-        BlocksIterator<D>(data, rate)
-    }
-}
-
-struct BlocksIterator<D>: IteratorProtocol where D: DataProtocol {
-    var data: D.SubSequence
-    let rate: Int
-    var firstBlock = true
-    
-    init(_ data: D, _ rate: Int) {
-        self.data = data.prefix(Int.max)
-        self.rate = rate
-    }
-    
-    mutating func next() -> D.SubSequence? {
-        if data.isEmpty && !firstBlock {
-            return nil
-        }
-        firstBlock = false
-        let block = data.prefix(min(data.count, rate))
-        data = data.suffix(data.count - block.count)
-        return block
+        .init(tail: data[...], rate: rate)
     }
 }
